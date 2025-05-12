dddff0c8ceb1cd039ca3478145a2004309122e98
caesaralpha <caesaralpha1994@gmail.com>
2025-05-12 22:02:34 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+# Add the project root to sys.path
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+# Import the transcribe_video function from the tools module
 from tools import transcribe_video
 
 def main():
