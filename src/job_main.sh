989538d8726f2817c91b4afecf2c5e048a2ba1c2
caesaralpha <caesaralpha1994@gmail.com>
2025-05-12 21:55:39 +0200

    Create job_main.sh

---
+++ src/job_main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=dt-tools
+#SBATCH --output=logs/dt-tools-%j.out
+#SBATCH --error=logs/dt-tools-%j.err
+
+#SBATCH --gpus 1
+#SBATCH -p gpu_h100
+
+#SBATCH --time=00:10:00  
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=c.caesaralphairawan@students.uu.nl
+
+# Set the model name from the command line argument
+
+
+# Activate your conda environment
+source $HOME/dtenv/bin/activate
+echo ">>>>>>>>>>dtenv is activated <<<<<<<<<" 
+module load 2023
+# Run the Python script
+python /projects/prjs1547/dt-ai-tools/src/main.py
+
