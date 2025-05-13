da62202510857a9073aa8b82b130485cab4a8a28
caesaralpha <caesaralpha1994@gmail.com>
2025-05-13 17:45:07 +0200

    Update job_main.sh

--- src/job_main.sh
+++ src/job_main.sh
@@ -16,7 +16,7 @@ echo ">>>>>>>>>>Modules loaded <<<<<<<<<"
 
 # Activate your conda environment
 source /projects/prjs1547/dtenv/bin/activate
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+# pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 echo ">>>>>>>>>>dtenv is activated <<<<<<<<<" 
 
 
