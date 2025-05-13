d87d35fb42a8fbc43c171dbff592339de11c6c53
caesaralpha <caesaralpha1994@gmail.com>
2025-05-13 22:15:43 +0200

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,3 @@
-from .video_transcriber import transcribe_video
+from .video_transcriber import transcribe_video_or_audio
+from .docling import convert_docs_to_markdown
 # Add imports for other tools as needed
